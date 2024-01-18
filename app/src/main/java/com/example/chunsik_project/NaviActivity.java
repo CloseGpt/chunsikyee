6fcdabf12ac20d358acf6558daccf597a0b6e6bd
Hyowon Kang <hyowon3434@naver.com>
2024-01-18 16:02:43 +0900

    Fix: 액션바 수정

--- app/src/main/java/com/example/chunsik_project/NaviActivity.java
+++ app/src/main/java/com/example/chunsik_project/NaviActivity.java
@@ -1,14 +1,32 @@
 package com.example.chunsik_project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class NaviActivity extends AppCompatActivity {
 
+    HomeFragment homeFragment;
+    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        fragmentManager =getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        homeFragment = new HomeFragment();
+        fragmentTransaction.replace(R.id.mainFrameLayout, homeFragment);
+        fragmentTransaction.commit();
+
     }
 }
