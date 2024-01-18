ca88f2510bf1441c48c0eb2da66489ccfa1da076
kevinkim <nuokim@tukorea.ac.kr>
2024-01-18 16:57:48 +0900

    홈 연동

--- app/src/main/java/com/example/chunsik_project/MainActivity.java
+++ app/src/main/java/com/example/chunsik_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chunsik_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // HomeFragment 인스턴스 생성
+        HomeFragment homeFragment = new HomeFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, homeFragment);
+        transaction.commit();
     }
 }
